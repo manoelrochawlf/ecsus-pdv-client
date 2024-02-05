83d87bcf5eb76fdd005849dc7a8b1264868a06b7
Manoel Rocha <manoelrocha399@gmail.com>
2024-02-05 16:55:20 -0300

    added react

---
+++ app/src/components/Layout.tsx
@@ -0,0 +1,9 @@
+const Layot = () => {
+  return (
+  <div>
+    <p>This is your Layout</p>
+  </div>
+   );
+}
+
+export default Layot;
--- main.js
+++ main.js
@@ -14,9 +14,11 @@ function createMainWindow() {
 
   });
 
+  mainWindow.webContents.openDevTools();
+
 
   const startUrl = url.format({
-    pathname:  path.join(__dirname, 'index.html'),
+    pathname:  path.join(__dirname, './app/build/index.html'),
     protocol: 'file',
   })
 
